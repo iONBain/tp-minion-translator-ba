f1f0f95c1110f8c924ede37dca16994aa3eab7fd
Bhaskar Agrawal <8dbdown@gmail.com>
2022-08-07 10:49:08 +0530

    update: app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-const serverURL = "https://api.funtranslations.com/translate/minion"
+const serverURL = "https://api.funtranslations.com/translate/minion.json"
 const inputTxt = document.getElementById("input")
-const ouputTxt = document.getElementById("ouput")
+const ouputTxt = document.getElementById("output")
 const translateBtn = document.getElementById("translate")
 const refreshBtn = document.getElementById("refresh")
 
@@ -9,17 +9,17 @@ function getURL(inputt){
 
 }
 function errorHandler(error) {
-    // console.log("error occured", error);
+    console.log("error occured", error);
     alert("something wrong with server ! try again later");
 }
 function translateTxt(){
     // console.log("Hello")
-    // console.log(inputTxt.value)
+    console.log(inputTxt.value)
     var url = getURL(inputTxt.value);
     fetch(url).then(res => res.json()).then( json => {
         console.log(json.contents.translated);
         ouputTxt.value = json.contents.translated;
-    }).catch(errorHandler)
+    }).catch(errorHandler);
 }
 
 
@@ -35,3 +35,4 @@ translateBtn.addEventListener("click", translateTxt)
 
 
 // https://api.funtranslations.com/translate/minion?text=Hi I am bhaskar
+// https://api.funtranslations.com/translate/minion.json?text=Hi I am bhaskar
